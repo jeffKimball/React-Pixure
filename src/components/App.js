b08a20dd76762a9bcfb38a64497266a78e04fc43
Jeff Kimball <jeffkimballis@gmail.com>
2021-10-28 17:44:57 -0500

    set up initial files

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SearchBar from './SearchBar'
+
+const App = (props) => {
+   console.log(props)
+    
+        return <div><SearchBar/></div>
+         
+}
+
+export default App
